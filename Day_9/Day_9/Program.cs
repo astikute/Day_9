a75cd569115737cf9635b18781d49cf26a2e83ac
astikute <agnese.stikute@gmail.com>
2018-05-17 20:09:03 +0300

    Izveidota klase

---
+++ Day_9/Day_9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_9
+{
+    class Program
+    {
+        static void Main(string[] args) // static, jo mēs no šīs klases netaisam objektu
+        {
+            //Objektu izveido, lai piekļūtu klasei un funkcijai
+            Test TestaObjekts = new Test();
+            int rezultats = TestaObjekts.Saskaitisana(3,7);
+
+            Console.WriteLine("Izvade no klases: " + rezultats);
+
+            Test TestaObjekts2 = new Test(); // šis jau ir cits objekts
+
+            TestaObjekts.skaitlis = 3;
+            TestaObjekts2.skaitlis = 6;
+            Console.WriteLine("Izvade no TestaObjekts: " + TestaObjekts.skaitlis);
+            Console.WriteLine("Izvade no TestaObjekts2: " + TestaObjekts2.skaitlis);
+
+            Console.Read();
+        }
+    }
+}
---
+++ Day_9/Day_9/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_9 //šeit static neparādās!
+    //namespace jābūt vienādiem
+{
+    class Test
+    {
+        //public (piekļūst ārpus Test faila) un private (izmanto tikai klasē Test)
+        public int skaitlis; //mainīgais
+
+        public int Saskaitisana(int a, int b)
+        {
+            Izvade();
+            return a + b;
+        }
+
+        private void Izvade()
+        {
+            Console.WriteLine("tests!");
+        }
+    }
+}
